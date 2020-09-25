74fa92223b04031769a9a97e0b0b5cd60e621dac
eitchtee <flaips.2@gmail.com>
2020-09-24 23:55:39 -0300

    feat: New logic for sync

--- gcal2todoist.py
+++ gcal2todoist.py
@@ -1,10 +1,11 @@
+import signal
 from time import sleep
 
 import todoist
-import os
-import yaml
-import signal
 from gcsa.google_calendar import GoogleCalendar
+import yaml
+import os
+from tinydb import TinyDB, Query
 
 
 class GracefulKiller:
@@ -27,79 +28,183 @@ class Configs:
         self.token = data.get('todoist_api_token')
         self.project = data.get('default_project')
         self.label = data.get('label')
+        self.keep_running = data.get('keep_running')
         self.run_every = data.get('run_every')
 
+        self.todoist_api = todoist.TodoistAPI(self.token)
+        self.calendar = GoogleCalendar(credentials_path=os.path.join(
+            os.path.dirname(__file__),
+            '.credentials', 'credentials.json'))
+        self.db = TinyDB('events.json')
 
-def fetch_project(api_obj):
-    matching_projects = [project for project in api_obj.state['projects'] if
+        self.project_id = None
+        self.label_id = None
+
+    def refresh_calendar(self):
+        self.calendar = GoogleCalendar(credentials_path=os.path.join(
+            os.path.dirname(__file__),
+            '.credentials', 'credentials.json'))
+
+
+def fetch_project_id():
+    matching_projects = [project for project
+                         in cf.todoist_api.state['projects'] if
                          project['name'] == Configs().project]
     if len(matching_projects) >= 1:
         proj_id = matching_projects[0]['id']
     else:
-        new_project = api_obj.projects.add(Configs().project)
-        api_obj.commit()
+        new_project = cf.todoist_api.projects.add(Configs().project)
+        cf.todoist_api.commit()
         proj_id = new_project['id']
 
     return proj_id
 
 
-def fetch_label(api_obj):
-    matching_labels = [label for label in api_obj.state['labels'] if
+def fetch_label_id():
+    matching_labels = [label for label in cf.todoist_api.state['labels'] if
                        label['name'] == Configs().label]
     if len(matching_labels) >= 1:
         r_label_id = matching_labels[0]['id']
     else:
-        new_label = api_obj.labels.add(Configs().label)
-        api_obj.commit()
+        new_label = cf.todoist_api.labels.add(Configs().label)
+        cf.todoist_api.commit()
         r_label_id = new_label['id']
 
     return r_label_id
 
 
-def add_event(api_obj, task, date, description, location, label_id, proj_id):
-    task = "* 🗓️ **" + task + '**'
-    item = api_obj.items.add(content=task,
-                             project_id=proj_id,
-                             labels=[label_id],
-                             due={'date': str(date),
-                                  'is_recurring': False,
-                                  'lang': 'pt',
-                                  'timezone': None})
-    api_obj.notes.add(item_id=item['id'],
-                      content=location) if location else None
-    api_obj.notes.add(item_id=item['id'],
-                      content=description) if description else None
+def add_event(event):
+    api = cf.todoist_api
+    task = "* 🗓️ **" + event.summary + '**'
 
+    item = api.items.add(content=task,
+                         project_id=cf.project_id,
+                         labels=[cf.label_id],
+                         due={'string': str(event.start),
+                              'is_recurring': False,
+                              'lang': 'pt',
+                              'timezone': None})
 
-def clear_events(api_obj, proj_id, lab_id):
-    for task in api_obj.state['items']:
-        if task['project_id'] == proj_id and lab_id in task['labels']:
-            print(task['labels'])
-            item = api_obj.items.get_by_id(task['id'])
-            item.delete()
-    api_obj.commit()
+    if event.location:
+        new_note = api.notes.add(item_id=item['id'],
+                                 content=event.location)
+        note_location = new_note
+    else:
+        note_location = {'id': None}
+
+    if event.description:
+        new_note = api.notes.add(item_id=item['id'],
+                                 content=event.description)
+        note_description = new_note
+    else:
+        note_description = {'id': None}
+
+    return {'item': item,
+            'note_location': note_location,
+            'note_description': note_description,
+            'event_id': event.id}
 
 
 def main():
-    api = todoist.TodoistAPI(Configs().token)
-    api.sync()
-    project_id = fetch_project(api)
-    label_id = fetch_label(api)
+    db = cf.db
+    todoist_api = cf.todoist_api
 
-    clear_events(api, project_id, label_id)
+    # Create db entry for all event_ids not already on
+    items_n_notes = []
+    for event in cf.calendar:
+        if not db.search(Query().event_id == event.id):
+            items_n_notes.append(add_event(event))
 
-    calendar = GoogleCalendar(credentials_path=os.path.join(
-        os.path.dirname(__file__), '.credentials', 'credentials.json'))
+    todoist_api.commit()
 
-    for event in calendar:
-        add_event(api, event.summary, event.start, event.description,
-                  event.location, label_id, project_id)
+    db.insert_multiple({'event_id': x['event_id'],
+                        'task_id': x['item']['id'],
+                        'note_ids': [x['note_location']['id'],
+                                     x['note_description']['id']]} for x in
+                       items_n_notes)
 
-    api.commit()
+    all_event_ids = [x.id for x in cf.calendar]
+    for entry in db:
+        # Skip checking if a event occurs during multiple days as it will be
+        # handled elsewhere
+        # TO-DO: Implement this handling
+        if isinstance(entry['task_id'], list):
+            continue
+
+        # Remove old entries and complete tasks
+        if entry['event_id'] not in all_event_ids:
+            todoist_api.items.complete(entry.get('task_id'))
+            db.remove(Query().event_id == entry['event_id'])
+            continue  # Skip updating as the event is over
+
+        event = [x for x in cf.calendar if
+                 x.id == entry['event_id']][0]
+
+        # Re-add missing task_id-event
+        if not todoist_api.items.get(entry['task_id']):
+            items_n_notes = add_event(event)
+            todoist_api.commit()
+            db.insert({'event_id': items_n_notes['event_id'],
+                       'task_id': items_n_notes['item']['id'],
+                       'note_ids': [items_n_notes['note_location']['id'],
+                                    items_n_notes['note_description']['id']]})
+
+        # Update task_id names
+        task_name = "* 🗓️ **" + event.summary + '**'
+        if todoist_api.items.get(
+                entry['task_id'])['item']['content'] != task_name:
+            todoist_api.items.update(entry['task_id'], content=task_name)
+
+        # Update due dates
+        if todoist_api.items.get(
+                entry['task_id'])['item']['due']['string'] != str(event.start):
+            todoist_api.items.update(entry['task_id'],
+                                     due={'string': str(event.start)})
+
+        # Update and delete notes
+        for i, note_id in enumerate(entry['note_ids']):
+            if note_id and todoist_api.notes.get(note_id):
+                note = todoist_api.notes.get(note_id)
+
+                if i == 0:
+                    if event.location and \
+                            event.location != note['note']['content']:
+                        todoist_api.notes.update(note_id,
+                                                 content=event.location)
+                    else:
+                        todoist_api.notes.delete(note_id)
+                elif i == 1:
+                    if event.description and \
+                            event.description != note['note']['content']:
+                        todoist_api.notes.update(note_id,
+                                                 content=event.description)
+                    else:
+                        todoist_api.notes.delete(note_id)
+
+    todoist_api.commit()
 
 
 if __name__ == '__main__':
-    killer = GracefulKiller()
-    while not killer.kill_now:
+    cf = Configs()
+
+    if cf.keep_running:
+        killer = GracefulKiller()
+        while not killer.kill_now:
+            cf.refresh_calendar()
+            cf.todoist_api.sync()
+
+            cf.project_id = fetch_project_id()
+            cf.label_id = fetch_label_id()
+
+            main()
+            sleep(cf.run_every)
+    else:
+        cf.refresh_calendar()
+        cf.todoist_api.sync()
+
+        cf.project_id = fetch_project_id()
+        cf.label_id = fetch_label_id()
+
         main()
-        sleep(Configs().run_every)
+
+    print('Goodbye!')
