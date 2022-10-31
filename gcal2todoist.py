5ee5f0daf5dbea0818fa98c85c547a34c66b9426
Herculino Trotta <netotrotta@gmail.com>
2022-10-31 14:29:38 -0300

    fix: only get next 15 days of events (reduce api calls)

--- gcal2todoist.py
+++ gcal2todoist.py
@@ -65,7 +65,7 @@ class Configs:
                 time_min=datetime.datetime.today(),
                 time_max=datetime.datetime.today()
                 + datetime.timedelta(
-                    days=31,
+                    days=15,
                 ),
                 single_events=True,
             )
